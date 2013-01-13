6fe67ac256e326b743e3548af7e76d7b3fead048
Tapasya Patki <tpatki@email.arizona.edu>
2013-01-12 18:39:29 -0700

    Commit.

--- bmarks/make-and-sub-sp.sh
+++ bmarks/make-and-sub-sp.sh
@@ -7,16 +7,16 @@ export PATH_LIBMSR=$curr/librapl-master/libmsr/lib
 #export PATH_LIBMSR=$curr/rapl-bmarks/bmarks/librapl/libmsr/lib
 echo $PATH_LIBMSR
 
-cd librapl-master/libmsr
-mkdir -p lib
-make clean && make -j8
+#cd librapl-master/libmsr
+#mkdir -p lib
+#make clean && make -j8
 
-cd mpi
-make clean && make -j8 rapl-icc
+#cd mpi
+#make clean && make -j8 rapl-icc
 
-cd ../../../NAS-MZ/
-make clean
-mkdir -p bin
+cd NAS-MZ/
+#make clean
+#mkdir -p bin
 sh makeSP_MZ.sh
 
 cd SP-MZ/power
