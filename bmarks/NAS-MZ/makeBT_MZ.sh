ea30956478bc64fb80a93558d783957eba4a7cd8
Peter Bailey <peter.eldridge.bailey@gmail.com>
2013-05-24 16:36:12 -0700

    Adding changes for make file to 4-48 nodes

--- bmarks/NAS-MZ/makeBT_MZ.sh
+++ bmarks/NAS-MZ/makeBT_MZ.sh
@@ -2,11 +2,12 @@
 
 #mkdir bin
 
-make bt-mz CLASS=A NPROCS=16
+#make bt-mz CLASS=A NPROCS=16
 
-make -j8 bt-mz CLASS=C NPROCS=1
-make -j8 bt-mz CLASS=C NPROCS=2
+#make -j8 bt-mz CLASS=C NPROCS=1
+#make -j8 bt-mz CLASS=C NPROCS=2
 make -j8 bt-mz CLASS=C NPROCS=4
+make -j8 bt-mz CLASS=C NPROCS=6
 make -j8 bt-mz CLASS=C NPROCS=8
 make -j8 bt-mz CLASS=C NPROCS=10
 make -j8 bt-mz CLASS=C NPROCS=12
@@ -19,7 +20,15 @@ make -j8 bt-mz CLASS=C NPROCS=24
 make -j8 bt-mz CLASS=C NPROCS=26
 make -j8 bt-mz CLASS=C NPROCS=28
 make -j8 bt-mz CLASS=C NPROCS=30
-make -j8 bt-mz CLASS=C NPROCS=232
+make -j8 bt-mz CLASS=C NPROCS=32
+make -j8 bt-mz CLASS=C NPROCS=34
+make -j8 bt-mz CLASS=C NPROCS=36
+make -j8 bt-mz CLASS=C NPROCS=38
+make -j8 bt-mz CLASS=C NPROCS=40
+make -j8 bt-mz CLASS=C NPROCS=42
+make -j8 bt-mz CLASS=C NPROCS=44
+make -j8 bt-mz CLASS=C NPROCS=46
+make -j8 bt-mz CLASS=C NPROCS=48
 #make -j8 bt-mz CLASS=C NPROCS=64
 #make -j8 bt-mz CLASS=C NPROCS=128
 #make bt-mz CLASS=C NPROCS=256
