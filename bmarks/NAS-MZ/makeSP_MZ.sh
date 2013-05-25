6c9e010333802823575cf4e5cf09c1f612af6ad7
anjanasasidharan <anjanasasidharan@merlot.cs.arizona.edu>
2013-05-25 14:32:27 -0700

    Changes for 4-48 nodes

--- bmarks/NAS-MZ/makeSP_MZ.sh
+++ bmarks/NAS-MZ/makeSP_MZ.sh
@@ -7,6 +7,7 @@
 #make -j8 sp-mz CLASS=C NPROCS=1
 #make -j8 sp-mz CLASS=C NPROCS=2
 make -j8 sp-mz CLASS=C NPROCS=4
+make -j8 sp-mz CLASS=C NPROCS=6
 make -j8 sp-mz CLASS=C NPROCS=8
 make -j8 sp-mz CLASS=C NPROCS=10
 make -j8 sp-mz CLASS=C NPROCS=12
@@ -20,6 +21,14 @@ make -j8 sp-mz CLASS=C NPROCS=26
 make -j8 sp-mz CLASS=C NPROCS=28
 make -j8 sp-mz CLASS=C NPROCS=30
 make -j8 sp-mz CLASS=C NPROCS=32
+make -j8 sp-mz CLASS=C NPROCS=34
+make -j8 sp-mz CLASS=C NPROCS=36
+make -j8 sp-mz CLASS=C NPROCS=38
+make -j8 sp-mz CLASS=C NPROCS=40
+make -j8 sp-mz CLASS=C NPROCS=42
+make -j8 sp-mz CLASS=C NPROCS=44
+make -j8 sp-mz CLASS=C NPROCS=46
+make -j8 sp-mz CLASS=C NPROCS=48
 #make sp-mz CLASS=C NPROCS=64
 #make sp-mz CLASS=C NPROCS=128
 #make sp-mz CLASS=C NPROCS=256
